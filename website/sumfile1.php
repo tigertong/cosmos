9577fec6ee2e7f49c345d1db5886fdbb119be026
Tiger <tiger.tong@microsoft.com>
2016-09-19 16:28:25 +0800

    add liner regression demo

---
+++ website/sumfile1.php
@@ -0,0 +1,469 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>stats</title>
+	<link rel="stylesheet" type="text/css" href="./css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="./css/style.css">
+<script src="./css/hm.js"></script><script src="./css/share.js"></script><link rel="stylesheet" href="./css/share_style0_24.css"></head>
+		<!--script type="text/javascript" src="http://cdn.hcharts.cn/jquery/jquery-1.8.3.min.js"></script-->
+		
+		<!-- Bootstrap -->
+			<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+			<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+			<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+	
+		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+		<script src="Linear-Regression/bootstrap/js/jquery-1.11.3.min.js"></script>
+		<!-- Include all compiled plugins (below), or include individual files as needed -->
+		<script src="Linear-Regression/bootstrap/js/bootstrap.min.js"></script>
+		<script type="text/javascript"src="Linear-Regression/js/jquery.flot.min.js"></script>
+		<script type="text/javascript"src="Linear-Regression/js/regression.min.js"></script>
+		<script type="text/javascript" src="https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=AM_HTMLorMML-full"></script>
+				
+		
+		
+		<style type="text/css">
+${demo.css}
+		</style>
+	<?php
+$contentdate = $_GET["date"];	
+require_once("./db.conf.php");
+echo $_GET['submit'].$_GET['startdate'].$_GET["stopdate"];
+$startdate = "2016-08-05";
+$stopdate = "2016-09-05";
+
+if (isset($_GET["submit"]) && isset($_GET["startdate"]) && isset($_GET["stopdate"])) {
+$startdate = $_GET["startdate"];
+$stopdate = $_GET["stopdate"];
+
+
+
+$number=1;
+$result = mysqli_query($db,"SELECT *  FROM sumfileinfo where date>='$startdate'  and  date<='$stopdate' order by date asc");
+
+$categories="";
+
+$SRPV_filtered = "";
+$SRPV = "";
+$Impressions_filtered="";
+$Impressions="";
+$Clicks_filtered="";
+$Clicks="";
+$DSQ="";
+
+while($row = mysqli_fetch_array($result,MYSQLI_ASSOC))
+ {
+ 
+ /*
+ IP_COUNT INT(10),
+REFFER_COUNT INT(10),
+ROWQUERY_COUNT INT(10),
+UA_COUNT INT(10) 
+ */
+$SRPV_filtered .= "".$row['SRPV_filtered'].",";
+$SRPV  .= "".$row['SRPV'].",";
+$Impressions_filtered  .= "".$row['Impressions_filtered'].",";
+$Impressions  .= "".$row['Impressions'].",";
+$Clicks_filtered  .= "".$row['Clicks_filtered'].",";
+$Clicks  .= "".$row['Clicks'].",";
+$DSQ  .= "".$row['DSQ'].",";
+
+$categories	 .= "'".$row['date']."',";
+
+if($SRPV_filtered == NULL )$SRPV_filtered=0;
+if($SRPV == NULL)$SRPV=0;
+if($Impressions_filtered=="")$Impressions_filtered=0;
+if($Impressions=="")$Impressions=0;
+if($Clicks_filtered=="")$Clicks_filtered=0;
+if($Clicks=="")$Clicks=0;
+if($DSQ=="")$DSQ=0;
+
+
+
+
+$SRPV_Linear .= "[".$number.",".$row['SRPV']."],";
+$number++;
+}
+
+$SRPV_filtered = substr($SRPV_filtered ,0, strlen($SRPV_filtered)-1);
+$SRPV = substr($SRPV ,0, strlen($SRPV)-1);
+$Impressions_filtered = substr($Impressions_filtered ,0, strlen($Impressions_filtered)-1);
+$Impressions = substr($Impressions ,0, strlen($Impressions)-1);
+$Clicks_filtered = substr($Clicks_filtered ,0, strlen($Clicks_filtered)-1);
+$Clicks = substr($Clicks ,0, strlen($Clicks)-1);
+$DSQ = substr($DSQ ,0, strlen($DSQ)-1);
+$categories = substr($categories ,0, strlen($categories)-1);
+$SRPV_Linear = substr($SRPV_Linear ,0, strlen($SRPV_Linear)-1); 
+	//mysqli_close($db);
+
+$number=1;
+$result1 = mysqli_query($db,"SELECT *  FROM Sogou_data where date>='$startdate'  and  date<='$stopdate' order by date asc");
+
+
+
+$SRPV_Raw = "";
+$SRPV_Anti_fraud = "";
+$Impressions_Raw="";
+$Impressions_Anti_fraud="";
+$Clicks_Raw="";
+$Clicks_Anti_fraud="";
+
+
+while($row = mysqli_fetch_array($result1,MYSQLI_ASSOC))
+ {
+ /*
+ IP_COUNT INT(10),
+REFFER_COUNT INT(10),
+ROWQUERY_COUNT INT(10),
+UA_COUNT INT(10) 
+ */
+$SRPV_Raw .= "".$row['SRPV_Raw'].",";
+$SRPV_Anti_fraud  .= "".$row['SRPV_Anti_fraud'].",";
+$Impressions_Raw  .= "".$row['Impressions_Raw'].",";
+$Impressions_Anti_fraud  .= "".$row['Impressions_Anti_fraud'].",";
+$Clicks_Raw  .= "".$row['Clicks_Raw'].",";
+$Clicks_Anti_fraud  .= "".$row['Clicks_Anti_fraud'].",";
+
+
+
+if($SRPV_Raw == NULL )$SRPV_Raw=0;
+if($SRPV_Anti_fraud == NULL)$SRPV_Anti_fraud=0;
+if($Impressions_Raw=="")$Impressions_Raw=0;
+if($Impressions_Anti_fraud=="")$Impressions_Anti_fraud=0;
+if($Clicks_Raw=="")$Clicks_Raw=0;
+if($Clicks_Anti_fraud=="")$Clicks_Anti_fraud=0;
+
+$SRPV_Raw_Linear .= "[".$number.",".$row['SRPV_Raw']."],";
+$number++;
+
+}
+
+$SRPV_Raw = substr($SRPV_Raw ,0, strlen($SRPV_Raw)-1);
+$SRPV_Anti_fraud = substr($SRPV_Anti_fraud ,0, strlen($SRPV_Anti_fraud)-1);
+$Impressions_Raw = substr($Impressions_Raw ,0, strlen($Impressions_Raw)-1);
+$Impressions_Anti_fraud = substr($Impressions_Anti_fraud ,0, strlen($Impressions_Anti_fraud)-1);
+$Clicks_Raw = substr($Clicks_Raw ,0, strlen($Clicks_Raw)-1);
+$Clicks_Anti_fraud = substr($Clicks_Anti_fraud ,0, strlen($Clicks_Anti_fraud)-1);
+$SRPV_Raw_Linear = substr($SRPV_Raw_Linear ,0, strlen($SRPV_Raw_Linear)-1); 
+	//mysqli_close($db);
+
+
+}
+	
+	?>		
+		
+		
+		<script type="text/javascript" >
+
+$(function () {
+
+	var SRPV_data = [<?php echo $SRPV_Linear ;?>];
+	var SRPV_myRegression = regression('linear', SRPV_data);
+ 	var SRPV_linearArr= new Array();	
+			
+	for(var i=0;i < SRPV_myRegression.points.length ;i++){
+		 SRPV_linearArr[i] = Math.round(SRPV_myRegression.points[i][1]);
+	}
+	
+	var SRPV_Raw_data = [<?php echo $SRPV_Raw_Linear ;?>];
+	var SRPV_Raw_myRegression = regression('linear', SRPV_Raw_data);
+ 	var SRPV_Raw_linearArr= new Array();	
+			
+	for(var i=0;i < SRPV_Raw_myRegression.points.length ;i++){
+		 SRPV_Raw_linearArr[i] = Math.round(SRPV_Raw_myRegression.points[i][1]);
+	}
+	
+    $('#container').highcharts({
+        chart: {
+            type: 'line'
+        },
+        title: {
+            text: ' 总搜索量(Raw SRPV) '
+        },
+        subtitle: {
+            text: ' SRPV/Sougou'
+        },
+        xAxis: {
+            categories: [<?php echo $categories;?>]//['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+        },
+        yAxis: {
+            title: {
+                text: 'request'
+            }
+        },
+        plotOptions: {
+            line: {
+                dataLabels: {
+                    enabled: true
+                },
+                enableMouseTracking: false
+            }
+        },
+        series: [{
+            name: 'SML',
+            data: [<?php echo $SRPV;?>] //[7.0, 6.9, 9.5, 14.5, 18.4, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
+        },{
+            name: 'Sogou',
+            data: [<?php echo $SRPV_Raw;?> ] //[7.0, 6.9, 9.5, 14.5, 18.4, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
+        },{
+            name: 'SML Linear',
+            data: SRPV_linearArr
+			//[44987035.03636363,44635067.87272727,44283100.7090909,43931133.54545454,43579166.381818175,43227199.21818181,42875232.05454545,42523264.89090908,42171297.72727272,41819330.56363636]
+        },{
+            name: 'Sogou Linear',
+            data: SRPV_Raw_linearArr
+        }
+		
+		]
+    });
+});
+$(function () {
+    $('#container1').highcharts({
+        chart: {
+            type: 'line'
+        },
+        title: {
+            text: ' SRPV_filtered '
+        },
+        subtitle: {
+            text: ' SRPV_filtered/Sougou/DSQ'
+        },
+        xAxis: {
+            categories: [<?php echo $categories;?>]//['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+        },
+        yAxis: {
+            title: {
+                text: 'request'
+            }
+        },
+        plotOptions: {
+            line: {
+                dataLabels: {
+                    enabled: true
+                },
+                enableMouseTracking: false
+            }
+        },
+        series: [
+		{
+            name: 'SML',
+            data: [<?php echo $SRPV_filtered;?>]  
+        },{
+            name: 'DSQ',
+            data: [<?php echo $DSQ;?>]  
+        },{
+            name: 'Sogou',
+            data: [<?php echo $SRPV_Anti_fraud;?>]  
+        }
+		
+		]
+    });
+});
+
+
+
+$(function () {
+    $('#containerIP').highcharts({
+        chart: {
+            type: 'line'
+        },
+        title: {
+            text: 'Raw Impressions'
+        },
+        subtitle: {
+            text: ' Impressions/Sougou'
+        },
+        xAxis: {
+            categories: [<?php echo $categories;?>]//['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+        },
+        yAxis: {
+            title: {
+                text: 'request'
+            }
+        },
+        plotOptions: {
+            line: {
+                dataLabels: {
+                    enabled: true
+                },
+                enableMouseTracking: false
+            }
+        },
+        series: [{
+            name: 'SML',
+            data: [<?php echo $Impressions;?>] //[7.0, 6.9, 9.5, 14.5, 18.4, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
+        },{
+            name: 'Sogou',
+            data: [<?php echo $Impressions_Raw;?>] //[7.0, 6.9, 9.5, 14.5, 18.4, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
+        } 
+		
+		]
+    });
+});
+$(function () {
+    $('#containerIPF').highcharts({
+        chart: {
+            type: 'line'
+        },
+        title: {
+            text: ' Impressions_filtered '
+        },
+        subtitle: {
+            text: ' Impressions_filtered/Sougou'
+        },
+        xAxis: {
+            categories: [<?php echo $categories;?>]//['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+        },
+        yAxis: {
+            title: {
+                text: 'request'
+            }
+        },
+        plotOptions: {
+            line: {
+                dataLabels: {
+                    enabled: true
+                },
+                enableMouseTracking: false
+            }
+        },
+        series: [
+	{
+           name: 'Sogou',
+           data: [<?php echo $Impressions_Anti_fraud;?>]  
+       },
+	    {
+            name: 'SML',
+            data: [<?php echo $Impressions_filtered;?>]  
+        }
+		
+		]
+    });
+});
+
+
+$(function () {
+    $('#containerC').highcharts({
+        chart: {
+            type: 'line'
+        },
+        title: {
+            text: 'Raw Clicks'
+        },
+        subtitle: {
+            text: ' Clicks/Sougou'
+        },
+        xAxis: {
+            categories: [<?php echo $categories;?>]//['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+        },
+        yAxis: {
+            title: {
+                text: 'request'
+            }
+        },
+        plotOptions: {
+            line: {
+                dataLabels: {
+                    enabled: true
+                },
+                enableMouseTracking: false
+            }
+        },
+        series: [{
+            name: 'SML',
+            data: [<?php echo $Clicks;?>] //[7.0, 6.9, 9.5, 14.5, 18.4, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
+        },{
+            name: 'Sogou',
+            data: [ <?php echo $Clicks_Raw; ?>] //[7.0, 6.9, 9.5, 14.5, 18.4, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
+        } 
+		
+		]
+    });
+});
+$(function () {
+    $('#containerCF').highcharts({
+        chart: {
+            type: 'line'
+        },
+        title: {
+            text: ' Clicks_filtered '
+        },
+        subtitle: {
+            text: ' Clicks_filtered/Sougou'
+        },
+        xAxis: {
+            categories: [<?php echo $categories;?>]//['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+        },
+        yAxis: {
+            title: {
+                text: 'request'
+            }
+        },
+        plotOptions: {
+            line: {
+                dataLabels: {
+                    enabled: true
+                },
+                enableMouseTracking: false
+            }
+        },
+        series: [
+		{
+            name: 'Sogou',
+            data: [<?php echo $Clicks_Anti_fraud;?>]  
+       },
+	    {
+            name: 'SML',
+            data: [<?php echo $Clicks_filtered;?>]  
+        }
+		
+		]
+    });
+});
+
+		</script>
+<script src="http://cdn.hcharts.cn/highcharts/highcharts.js"></script>
+<script src="http://cdn.hcharts.cn/highcharts/modules/exporting.js"></script>
+
+</head>
+
+<body>
+<div id="demo">
+  <div id="demo_content">
+    <div class="page-header">
+      <h1>sougou traffic SumFile trend</h1>
+      <div class="clear"></div>
+    </div>
+    <p> 图表主题：
+       <a href="IP.php"  > <button class="btn" theme="default">IP distribution</button></a>
+      <a href="IPList.php" > <button class="btn" theme="grid">traffic trend</button></a>
+      <a href="UAList.php" > <button class="btn" theme="grid">UA distribution</button></a>
+      <a href="RFList.php" > <button class="btn" theme="grid">RF distribution</button></a>
+      <a href="QUList.php" > <button class="btn" theme="grid">QUERY distribution</button></a>
+	  	  <a href="fromCode.php" > <button class="btn" theme="grid">From Code distribution</button></a>
+<a href="sumfile.php" > <button class="btn" theme="grid">SumFile distribution</button></a>
+<a href="sougou.php" > <button class="btn" theme="grid">sougou distribution</button></a>
+    </p><p>
+	<form   action='sumfile.php' method='get'>
+	startdate:<input type="text" id="startdate" name="startdate" size="20" value="<?php echo $startdate ; ?>"><br> 
+	stopdate:<input type="text" id="stopdate" name="stopdate" size="20" value="<?php echo $stopdate ; ?>"><br><input name="submit" value="submit" id="submit"  type="submit" />
+	</form>
+	</p>
+    <div id="container" style="min-width: 310px; height: 400px; margin: 0 auto"></div>
+	<div id="container1" style="min-width: 310px; height: 400px; margin: 0 auto"></div>
+      <div id="containerIP" style="min-width: 310px; height: 400px; margin: 0 auto"></div>
+	<div id="containerIPF" style="min-width: 310px; height: 400px; margin: 0 auto"></div>
+      <div id="containerC" style="min-width: 310px; height: 400px; margin: 0 auto"></div>
+	<div id="containerCF" style="min-width: 310px; height: 400px; margin: 0 auto"></div>
+  </div>
+
+</div>
+<p>&nbsp;</p>
+</body>
+</html>
