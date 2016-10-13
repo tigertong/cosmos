46159070663277aab87cff74ffb1addc2d26c63c
Tiger <tiger.tong@microsoft.com>
2016-10-13 11:08:15 +0800

    coding

--- website/logdumpOhterData.php
+++ website/logdumpOhterData.php
@@ -12,27 +12,26 @@ $datemonth = $argv[1];
 $date = $argv[2];
 }
 
-$date = $_GET['date'];
-$datemonth = $_GET['datemonth'];
+//$date = $_GET['date'];
+//$datemonth = $_GET['datemonth'];
 
 
 //$_localpath="C:/BingAds";
-$_localpath="C:/Work_Env/BingAds";
+$_localpath="C:/BingAds";
 $_logfolder="$_localpath/logs/";
 $_logfolder_done="$_localpath/logs_done/";
 
- 
-$command_url0 = " $_localpath/tools/ScopeSDK/Scope.exe copy     https://cosmos08.osdinfra.net/cosmos/bingads.marketplace.VC1/users/tigertong/sogouads/2016result/FCDistribution_$date.csv	                        $_localpath/logs/FCDistribution_$date.csv	                   ";
+/*$command_url0 = " $_localpath/tools/ScopeSDK/Scope.exe copy     https://cosmos08.osdinfra.net/cosmos/bingads.marketplace.VC1/users/tigertong/sogouads/2016result/FCDistribution_$date.csv	                        $_localpath/logs/FCDistribution_$date.csv	                   ";
 $command_url1 = " $_localpath/tools/ScopeSDK/Scope.exe copy     https://cosmos08.osdinfra.net/cosmos/bingads.marketplace.VC1/users/tigertong/sogouads/2016result/IPSumFile_$date.csv	                            $_localpath/logs/IPSumFile_$date.csv	                       ";
 $command_url2 = " $_localpath/tools/ScopeSDK/Scope.exe copy     https://cosmos08.osdinfra.net/cosmos/bingads.marketplace.VC1/users/tigertong/sogouads/2016result/QUDistribution_$date.csv	                        $_localpath/logs/QUDistribution_$date.csv	                   ";
 $command_url3 = " $_localpath/tools/ScopeSDK/Scope.exe copy     https://cosmos08.osdinfra.net/cosmos/bingads.marketplace.VC1/users/tigertong/sogouads/2016result/RFDistribution_$date.csv	                        $_localpath/logs/RFDistribution_$date.csv	                   ";
-$command_url4 = " $_localpath/tools/ScopeSDK/Scope.exe copy     https://cosmos08.osdinfra.net/cosmos/bingads.marketplace.VC1/users/tigertong/sogouads/2016result/RFDomainDistribution_$date.csv	                $_localpath/logs/RFDomainDistribution_$date.csv	               ";
+$command_url4 = " $_localpath/tools/ScopeSDK/Scope.exe copy     https://cosmos08.osdinfra.net/cosmos/bingads.marketplace.VC1/users/tigertong/sogouads/2016result/RFDomainDistribution_$date.csv	                    $_localpath/logs/RFDomainDistribution_$date.csv	               ";
 $command_url5 = " $_localpath/tools/ScopeSDK/Scope.exe copy     https://cosmos08.osdinfra.net/cosmos/bingads.marketplace.VC1/users/tigertong/sogouads/2016result/State_GROUPBY_Distribution_$date.csv	            $_localpath/logs/State_GROUPBY_Distribution_$date.csv	       ";
 $command_url6 = " $_localpath/tools/ScopeSDK/Scope.exe copy     https://cosmos08.osdinfra.net/cosmos/bingads.marketplace.VC1/users/tigertong/sogouads/2016result/TOP_GROUPBY_Location_Distribution_$date.csv	    $_localpath/logs/TOP_GROUPBY_Location_Distribution_$date.csv   ";
-$command_url7 = " $_localpath/tools/ScopeSDK/Scope.exe copy     https://cosmos08.osdinfra.net/cosmos/bingads.marketplace.VC1/users/tigertong/sogouads/2016result/TOP_IP_GROUPBY_Distribution_$date.csv	        $_localpath/logs/TOP_IP_GROUPBY_Distribution_$date.csv	       ";
-$command_url8 = " $_localpath/tools/ScopeSDK/Scope.exe copy     https://cosmos08.osdinfra.net/cosmos/bingads.marketplace.VC1/users/tigertong/sogouads/2016result/UADistribution_$date.csv                         $_localpath/logs/UADistribution_$date.csv                      ";
+$command_url7 = " $_localpath/tools/ScopeSDK/Scope.exe copy     https://cosmos08.osdinfra.net/cosmos/bingads.marketplace.VC1/users/tigertong/sogouads/2016result/TOP_IP_GROUPBY_Distribution_$date.csv	            $_localpath/logs/TOP_IP_GROUPBY_Distribution_$date.csv	       ";
+$command_url8 = " $_localpath/tools/ScopeSDK/Scope.exe copy     https://cosmos08.osdinfra.net/cosmos/bingads.marketplace.VC1/users/tigertong/sogouads/2016result/UADistribution_$date.csv                           $_localpath/logs/UADistribution_$date.csv                      ";
 
-/*echo $command_url0;  exec($command_url0);
+echo $command_url0;  exec($command_url0);
 echo $command_url1;  exec($command_url1);
 echo $command_url2;  exec($command_url2);
 echo $command_url3;  exec($command_url3);
@@ -41,6 +40,26 @@ echo $command_url5;  exec($command_url5);
 echo $command_url6;  exec($command_url6);
 echo $command_url7;  exec($command_url7);
 echo $command_url8;  exec($command_url8);*/
+
+$command_url0 = " $_localpath/tools/ScopeSDK/Scope.exe copy     https://cosmos08.osdinfra.net/cosmos/bingads.marketplace.VC1/users/tigertong/sogouads/2016result/FCDistribution_Filtered_$date.csv	                        $_localpath/logs/FCDistribution_Filtered_$date.csv	                   ";
+$command_url1 = " $_localpath/tools/ScopeSDK/Scope.exe copy     https://cosmos08.osdinfra.net/cosmos/bingads.marketplace.VC1/users/tigertong/sogouads/2016result/IPSumFile_Filtered_$date.csv	                            $_localpath/logs/IPSumFile_Filtered_$date.csv	                       ";
+$command_url2 = " $_localpath/tools/ScopeSDK/Scope.exe copy     https://cosmos08.osdinfra.net/cosmos/bingads.marketplace.VC1/users/tigertong/sogouads/2016result/QUDistribution_Filtered_$date.csv	                        $_localpath/logs/QUDistribution_Filtered_$date.csv	                   ";
+$command_url3 = " $_localpath/tools/ScopeSDK/Scope.exe copy     https://cosmos08.osdinfra.net/cosmos/bingads.marketplace.VC1/users/tigertong/sogouads/2016result/RFDistribution_Filtered_$date.csv	                        $_localpath/logs/RFDistribution_Filtered_$date.csv	                   ";
+$command_url4 = " $_localpath/tools/ScopeSDK/Scope.exe copy     https://cosmos08.osdinfra.net/cosmos/bingads.marketplace.VC1/users/tigertong/sogouads/2016result/RFDomainDistribution_Filtered_$date.csv	                    $_localpath/logs/RFDomainDistribution_Filtered_$date.csv	               ";
+$command_url5 = " $_localpath/tools/ScopeSDK/Scope.exe copy     https://cosmos08.osdinfra.net/cosmos/bingads.marketplace.VC1/users/tigertong/sogouads/2016result/State_GROUPBY_Distribution_Filtered_$date.csv	            $_localpath/logs/State_GROUPBY_Distribution_Filtered_$date.csv	       ";
+$command_url6 = " $_localpath/tools/ScopeSDK/Scope.exe copy     https://cosmos08.osdinfra.net/cosmos/bingads.marketplace.VC1/users/tigertong/sogouads/2016result/TOP_GROUPBY_Location_Distribution_Filtered_$date.csv	    $_localpath/logs/TOP_GROUPBY_Location_Distribution_Filtered_$date.csv   ";
+$command_url7 = " $_localpath/tools/ScopeSDK/Scope.exe copy     https://cosmos08.osdinfra.net/cosmos/bingads.marketplace.VC1/users/tigertong/sogouads/2016result/TOP_IP_GROUPBY_Distribution_Filtered_$date.csv	            $_localpath/logs/TOP_IP_GROUPBY_Distribution_Filtered_$date.csv	       ";
+$command_url8 = " $_localpath/tools/ScopeSDK/Scope.exe copy     https://cosmos08.osdinfra.net/cosmos/bingads.marketplace.VC1/users/tigertong/sogouads/2016result/UADistribution_Filtered_$date.csv                           $_localpath/logs/UADistribution_Filtered_$date.csv                      ";
+
+echo $command_url0;  exec($command_url0);
+echo $command_url1;  exec($command_url1);
+echo $command_url2;  exec($command_url2);
+echo $command_url3;  exec($command_url3);
+echo $command_url4;  exec($command_url4);
+echo $command_url5;  exec($command_url5);
+echo $command_url6;  exec($command_url6);
+echo $command_url7;  exec($command_url7);
+echo $command_url8;  exec($command_url8);
 	
 
 
@@ -49,41 +68,32 @@ require_once($_localpath."/CODE/website/db.conf.php");
 
 // Open a known directory, and proceed to read its contents
 
-if(file_exists("$_localpath/logs/FCDistribution_$date.csv")) {
-echo "-------FCDistribution_-------- <br>";
-	file_handle("$_localpath/logs/FCDistribution_$date.csv",$db);                                
+if(file_exists("$_localpath/logs/FCDistribution_Filtered_$date.csv")) {
+	file_handle("$_localpath/logs/FCDistribution_Filtered_$date.csv",$db);                                
 }     
-if(file_exists("$_localpath/logs/IPSumFile_$date.csv"))	{
-echo "-------IPSumFile_-------- <br>";
-	file_handle("$_localpath/logs/IPSumFile_$date.csv",$db);                                     
+if(file_exists("$_localpath/logs/IPSumFile_Filtered_$date.csv"))	{
+	file_handle("$_localpath/logs/IPSumFile_Filtered_$date.csv",$db);                                     
 }
-if(file_exists("$_localpath/logs/QUDistribution_$date.csv")){
-echo "-------QUDistribution_-------- <br>";
-	file_handle("$_localpath/logs/QUDistribution_$date.csv",$db);                               
+if(file_exists("$_localpath/logs/QUDistribution_Filtered_$date.csv")){
+	file_handle("$_localpath/logs/QUDistribution_Filtered_$date.csv",$db);                               
  }
-if(file_exists("$_localpath/logs/RFDistribution_$date.csv")){
-echo "-------RFDistribution_-------- <br>";
-	file_handle("$_localpath/logs/RFDistribution_$date.csv",$db);                                
+if(file_exists("$_localpath/logs/RFDistribution_Filtered_$date.csv")){
+	file_handle("$_localpath/logs/RFDistribution_Filtered_$date.csv",$db);                                
 }
-if(file_exists("$_localpath/logs/RFDomainDistribution_$date.csv")){
-echo "-------RFDomainDistribution_-------- <br>";
-	file_handle("$_localpath/logs/RFDomainDistribution_$date.csv",$db);                          
+if(file_exists("$_localpath/logs/RFDomainDistribution_Filtered_$date.csv")){
+	file_handle("$_localpath/logs/RFDomainDistribution_Filtered_$date.csv",$db);                          
 }
-if(file_exists("$_localpath/logs/State_GROUPBY_Distribution_$date.csv")){
-echo "-------State_GROUPBY_Distribution_-------- <br>";
-	file_handle("$_localpath/logs/State_GROUPBY_Distribution_$date.csv",$db);                   
+if(file_exists("$_localpath/logs/State_GROUPBY_Distribution_Filtered_$date.csv")){
+	file_handle("$_localpath/logs/State_GROUPBY_Distribution_Filtered_$date.csv",$db);                   
 }
-if(file_exists("$_localpath/logs/TOP_GROUPBY_Location_Distribution_$date.csv")){
-echo "-------TOP_GROUPBY_Location_Distribution_-------- <br>";
-	file_handle("$_localpath/logs/TOP_GROUPBY_Location_Distribution_$date.csv",$db);             
+if(file_exists("$_localpath/logs/TOP_GROUPBY_Location_Distribution_Filtered_$date.csv")){
+	file_handle("$_localpath/logs/TOP_GROUPBY_Location_Distribution_Filtered_$date.csv",$db);             
 }
-if(file_exists("$_localpath/logs/TOP_IP_GROUPBY_Distribution_$date.csv")){
-echo "-------TOP_IP_GROUPBY_Distribution_-------- <br>";
-	file_handle("$_localpath/logs/TOP_IP_GROUPBY_Distribution_$date.csv",$db);                   
+if(file_exists("$_localpath/logs/TOP_IP_GROUPBY_Distribution_Filtered_$date.csv")){
+	file_handle("$_localpath/logs/TOP_IP_GROUPBY_Distribution_Filtered_$date.csv",$db);                   
 }
-if(file_exists("$_localpath/logs/UADistribution_$date.csv")){
-echo "-------UADistribution_-------- <br>";
-	file_handle("$_localpath/logs/UADistribution_$date.csv",$db);                                
+if(file_exists("$_localpath/logs/UADistribution_Filtered_$date.csv")){
+	file_handle("$_localpath/logs/UADistribution_Filtered_$date.csv",$db);                                
 }
 
 function file_handle($filename,$db){
@@ -95,7 +105,7 @@ function file_handle($filename,$db){
 	$i=0;
 	foreach($arrConts as $cont){
 		if($cont== NULL) continue;
-		if($i>=1000) break;
+		if($i>=1000)break;
 		$cont = str_replace("\\","",$cont);
 		$cont = str_replace("'","\'",$cont);
 		$data = explode("	",$cont); 
